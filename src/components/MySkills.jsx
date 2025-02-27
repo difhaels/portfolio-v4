3b0c260aeebb81092eae036e0fe93043f0196a60
Agung Saputra <agungsaputradifh@gmail.com>
2025-02-27 08:41:18 +0700

    skills update

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { FaReact, FaNodeJs, FaFigma, FaDatabase } from "react-icons/fa";
-import {
-  SiNextdotjs,
-  SiTailwindcss,
-  SiExpress,
-  SiMongodb,
+import { FaReact, FaNodeJs, FaPhp, FaFigma, FaDatabase, FaVuejs } from "react-icons/fa";
+import { 
+  SiNextdotjs, 
+  SiTailwindcss, 
+  SiExpress, 
+  SiMongodb, 
+  SiMysql, 
+  SiFirebase, 
+  SiLaravel, 
+  SiCloudinary, 
+  SiRedux, 
+  SiTypescript, 
+  SiSass, 
+  SiJsonwebtokens 
 } from "react-icons/si";
 import Container from "./Container";
-import MovingText from "./MovingText";
 import Section from "./Section";
 import TitleText from "./TitleText";
 
@@ -19,11 +26,12 @@ export default function MySkills() {
       category: "Frontend Development",
       items: [
         { name: "React.js", icon: <FaReact className="text-sky-400" /> },
+        { name: "Vue.js", icon: <FaVuejs className="text-green-400" /> },
         { name: "Next.js", icon: <SiNextdotjs className="text-white" /> },
-        {
-          name: "Tailwind CSS",
-          icon: <SiTailwindcss className="text-cyan-400" />,
-        },
+        { name: "Redux", icon: <SiRedux className="text-purple-400" /> },
+        { name: "TypeScript", icon: <SiTypescript className="text-blue-400" /> },
+        { name: "Tailwind CSS", icon: <SiTailwindcss className="text-cyan-400" /> },
+        { name: "Sass", icon: <SiSass className="text-pink-500" /> },
       ],
     },
     {
@@ -31,23 +39,30 @@ export default function MySkills() {
       items: [
         { name: "Node.js", icon: <FaNodeJs className="text-green-400" /> },
         { name: "Express.js", icon: <SiExpress className="text-white" /> },
+        { name: "PHP", icon: <FaPhp className="text-blue-500" /> },
+        { name: "Laravel", icon: <SiLaravel className="text-red-500" /> },
+        { name: "JWT", icon: <SiJsonwebtokens className="text-white" /> },
       ],
     },
     {
-      category: "Database",
+      category: "Database & Storage",
       items: [
+        { name: "MySQL", icon: <SiMysql className="text-blue-500" /> },
         { name: "MongoDB", icon: <SiMongodb className="text-green-500" /> },
-        {
-          name: "Database Design",
-          icon: <FaDatabase className="text-yellow-400" />,
-        },
+        { name: "Firebase", icon: <SiFirebase className="text-yellow-400" /> },
+        { name: "Cloudinary", icon: <SiCloudinary className="text-gray-400" /> },
       ],
     },
     {
-      category: "UI/UX Design",
-      items: [{ name: "Figma", icon: <FaFigma className="text-pink-400" /> }],
+      category: "UI/UX & Others",
+      items: [
+        { name: "Figma", icon: <FaFigma className="text-pink-400" /> },
+        
+        
+      ],
     },
   ];
+
   return (
     <Section what={"section-skills"}>
       <Container>
