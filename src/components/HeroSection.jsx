de3cc606c37f6856f8f382ef9a8d7eb10175d714
Agung Saputra <agungsaputradifh@gmail.com>
2025-03-08 22:31:52 +0700

    priority

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,6 +3,7 @@ import { ArrowDown } from "lucide-react";
 import { Typewriter } from "react-simple-typewriter";
 import Sosmed from "./Sosmed";
 import "aos/dist/aos.css";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
@@ -17,26 +18,23 @@ export default function HeroSection() {
       }}
     >
       <div className="mx-auto mt-16 flex max-w-4xl flex-col items-center sm:mt-20">
-        <div
-          
-          className="relative mb-10 flex items-center justify-center"
-        >
+        import Image from "next/image";
+        <div className="relative mb-10 flex items-center justify-center">
           {/* Border Berputar */}
           <span className="absolute h-[156px] w-[156px] animate-spin rounded-full bg-gradient-to-tr from-[#72E2AE] to-transparent sm:h-[166px] sm:w-[166px]"></span>
 
           {/* Gambar Profil */}
           <div className="relative z-10 h-[156px] w-[156px] overflow-hidden rounded-full border-[6px] border-[#72E2AE]/20 sm:h-[166px] sm:w-[166px]">
-            <img
+            <Image
               src="/anime.jpeg"
               alt="Agung Saputra"
-              className="h-full w-full object-cover"
+              fill
+              className="object-cover"
+              priority
             />
           </div>
         </div>
-        <div
-          
-          className="mb-5 gap-2 text-4xl font-bold sm:flex"
-        >
+        <div className="mb-5 gap-2 text-4xl font-bold sm:flex">
           <h1>Hi, I am</h1>
           <span className="text-[#72E2AE]">
             <Typewriter
@@ -50,22 +48,17 @@ export default function HeroSection() {
             />
           </span>
         </div>
-        <p
-          
-          className="mx-5 mb-4 max-w-xl text-lg text-gray-400"
-        >
+        <p className="mx-5 mb-4 max-w-xl text-lg text-gray-400">
           I am a Full-Stack web developer. I can provide clean code and pixel
           perfect design. I also make website more & more interactive with web
           animations.
         </p>
-        <div >
+        <div>
           <Sosmed />
         </div>
       </div>
 
-      <div
-        className="mt-16 flex items-center gap-2 sm:mt-20"
-      >
+      <div className="mt-16 flex items-center gap-2 sm:mt-20">
         <ArrowDown className="h-4 animate-bounce text-gray-400" />
         <p className="text-xs font-semibold text-gray-400">SCROLL DOWN</p>
       </div>
